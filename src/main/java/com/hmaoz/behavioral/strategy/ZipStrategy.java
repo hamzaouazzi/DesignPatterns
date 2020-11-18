d07b32a5f81090f5c9312b64f2e06180a95acf68
HAMZA OUAZZI <ouazzihamza97@gmail.com>
2020-11-18 22:43:54 +0100

    added Strategy implementation

---
+++ src/main/java/com/hmaoz/behavioral/strategy/ZipStrategy.java
@@ -0,0 +1,14 @@
+package com.hmaoz.behavioral.strategy;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * 2 - Bury implementation
+ */
+public class ZipStrategy implements CompressionStrategy {
+    @Override
+    public void compress(List<File> files) {
+        System.out.println("Compressing files using to ZIP");
+    }
+}
