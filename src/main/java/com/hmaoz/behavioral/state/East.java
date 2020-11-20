98fe98774f1a22b7d6cacd081333f5411d8d99f2
HAMZA OUAZZI <ouazzihamza97@gmail.com>
2020-11-20 20:44:42 +0100

    added State implementation

---
+++ src/main/java/com/hmaoz/behavioral/state/East.java
@@ -0,0 +1,17 @@
+package com.hmaoz.behavioral.state;
+
+public class East implements State{
+    @Override
+    public void turnLeft(CompassContext compassContext) {
+        compassContext.setCompassState(new South());
+        System.out.println("Turning South");
+
+    }
+
+    @Override
+    public void turnRight(CompassContext compassContext) {
+        compassContext.setCompassState(new North());
+        System.out.println("Turning North");
+
+    }
+}
